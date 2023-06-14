ff7ec6154634c678bcc938b7b474c485e8b0f1e1
AnthonyNancy <tonyuche003@gmail.com>
2023-06-14 10:46:32 -0700

    The nigga is a song don

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * * free_grid -> removing memory allocation
@@ -11,6 +12,8 @@ void free_grid(int **grid, int height)
 	int i;
 
 	for (i = 0; i < height; i++)
+	{
 		free(grid[i]);
+	}
 	free(grid);
 }
