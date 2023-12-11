17c0bacd07d3debe8122ffacf7cd509fd48d5497
NancyAnthony <tonyuche003@gmail.com>
2023-12-11 15:52:37 -0800

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+/**
+ * sub - minus
+ * add - addition
+ * mul - multiply
+ * div - divide
+ * mod - module
+ * @k: integer
+ * @m: integer
+ *
+ * Return:result
+ */
+int sub(int k, int m)
+{
+        return (k - m);
+}
+int add(int k, int m)
+{
+        return (k + m);
+}
+int mul(int k, int m)
+{
+        return (k * m);
+}       
+int div(int k, int m)
+{       
+        return (k / m);
+}       
+int mod(int k, int m)
+{
+        return (k % m);
+}       
